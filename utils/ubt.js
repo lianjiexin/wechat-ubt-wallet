8d933e6d92d8a5b25af38c4d0d902cf0987a9499
jiexin lian <lianjiexin@hotmail.com>
2020-09-14 18:09:57 +0800

    update ubt.js to support exchange between ubt and score bi-directionaly

--- utils/ubt.js
+++ utils/ubt.js
@@ -3,174 +3,236 @@ Include all utility functions for UBT
 */
 const CONFIG = require('../config.js')
 
-async function checkAndCreateUser(uid) {
+async function checkAndCreateUser(uid)
+{
   return new Promise((resolve, reject) => {
-    retrieveUBT(uid, 'score').then(function (res) {
-      if (res.status == 0 && res.data == null) {
-        var ret = createAccount(uid, 10, 'score');
-        resolve(ret);
-      } else {
-        resolve(res);
-      }
-    })
+  retrieveUBT(uid,'score').then(function (res){
+  if(res.status == 0 && res.data == null){
+    console.info('Create User for ' + uid );
+    var ret = createAccount(uid,10,'score');
+ 
+    resolve(ret);     // to-be tested
+           }
+  else{
+
+        console.info("response not null, the user" + uid + "should have been created");
+        //reject ( new Error(res));
+        console.info(res);
+        resolve(res); 
+    }
+   })
+  })
+   
+}
+async function exchangeScoreToUBT(uid,ubtAddress, score)
+{
+  var ubt = 2 * score; // Assuming the exchange ratio is ubt/score = 2
+  return new Promise((resolve, reject) => {
+  
+    var decreaseParam = createScoreParam(uid,score,'score');
+    var increaseParam = createUBTParam(uid,ubtAddress,ubt);
+
+  decreaseUBT(decreaseParam).
+  then(increaseUBT(increaseParam)).
+  then(function (res){
+    console.info ('User ' + uid + ' : exchange score ' + score + ' for ubt: ' + ubt)
+    var ret = { 'uid' :uid,
+              'score':score,
+              'growth':ubt,
+              'status':0
+              }
+    resolve(ret);
+  });
+})
+
+}
+
+async function exchangeUBTtoScore(uid,ubtAddress, ubt)
+{
+  var score = ubt / 2; // Assuming the exchange ratio is score/ubt = 0.5
+  return new Promise((resolve, reject) => {
+
+    var decreaseParam = createUBTParam(uid,ubtAddress,ubt);
+    var increaseParam = createScoreParam(uid,score,'score');
+    
+  decreaseUBT(decreaseParam).
+  then(increaseUBT(increaseParam)).
+  then(function (res){
+    console.info ('User ' + uid + ' : exchange UBT ' + ubt + ' for score: ' + score)
+    var ret = { 'uid' :uid,
+              'score':score,
+              'growth':ubt,
+              'status':0
+              }
+    resolve(ret);
+    });
   })
 
 }
-async function exchangeScoreToUBT(uid, ubtAddress, score) {
-  var ubt = 2 * score; // Assuming the exchange ratio is growth/score = 2
-  return new Promise((resolve, reject) => {
-    decreaseUBT(uid, score, 'score').
-      then(increaseUBT(uid, ubtAddress, ubt, 'ubt')).
-      then(function (res) {
-        var ret = {
-          'uid': uid,
-          'score': score,
-          'growth': ubt,
-          'status': 0
-        }
-        resolve(ret);
-      });
-  })
 
-
-}
-
-async function retrieveUBT(uid, pointType) {
+async function retrieveUBT (uid,pointType)
+{
   var domain = CONFIG.ubtDomain
   return new Promise((resolve, reject) => {
-    wx.request({
-      url: 'http://' + domain + '/ubt/point/getPoint', //检查该用户的UBT；
-      data: {
+  wx.request({
+    url: 'http://' + domain +'/ubt/point/getPoint', //检查该用户的UBT；
+    data: {
         "type": pointType,
         "uid": uid
-      },
-      method: "GET",
-      header: {
-        "Content-Type": "application/json"
-      },
-      complete: function (res) {
-        if (res == null || res.data == null) {
+    },
+    method: "GET",
+    header: {
+        "Content-Type": "application/json" 
+    },
+    complete: function( res ) {
+        if( res == null || res.data == null) {
+          console.error('网络请求失败')
           reject(new Error('网络请求失败'))
         }
 
-      },
-      success: function (res) {
+    },
+    success: function(res) {
+        console.info ("Successfully Retrieve UBT for User: " + uid +', pointType:' + pointType);
+        console.info(res);
         resolve(res.data);
-      }
-    })
+    }
   })
+})
 }
 
-async function decreaseUBT(uid, point, pointType) {
+function createScoreParam(uid,point,pointType)
+{
+  return   {
+    "point": point,
+    "seq": Math.round(Math.random() * 1000000),
+    "type": pointType,
+    "uid": uid
+  }
+}
+
+function createUBTParam(uid,ubtAddress,point)
+{
+  return   {
+    "point": point,
+    "seq": Math.round(Math.random() * 1000000),
+    "type": 'ubt',
+    "uid": uid
+  }
+}
+async function decreaseUBT (requestParam)
+{
   var that = this;
   var domain = CONFIG.ubtDomain
   return new Promise((resolve, reject) => {
-    wx.request({
-      url: 'http://' + domain + '/ubt/point/decrease', //减少积分；
-      data: {
-        "point": point,
-        "seq": Math.round(Math.random() * 1000000),
-        "type": pointType,
-        "uid": uid
-      },
-      method: "POST",
-      header: {
-        "Content-Type": "application/json"
-      },
-      complete: function (res) {
-        if (res == null || res.data == null) {
+  wx.request({
+    url: 'http://' + domain +'/ubt/point/decrease', //减少积分；
+    data: requestParam,
+    method: "POST",
+    header: {
+        "Content-Type": "application/json" 
+    },
+    complete: function( res ) {
+        if( res == null || res.data == null) {
+          console.error('网络请求失败')
           reject(new Error('网络请求失败'))
         }
+        
 
-
-      },
-      success: function (res) {
-        if (res.data.status == 0) {
-          resolve(res.data)
-        }
-        else {
-          reject(new Error(res));
-        }
-      }
-    })
+    },
+    success: function(res) {
+      if(res.data.status == 0){
+        console.info('Successfully decrease Point ' );
+        console.info(requestParam);
+        resolve(res.data)
+    }
+    else
+    {
+      reject(new Error(res));
+    }
+    }
   })
+})
 }
 
 
-async function increaseUBT(uid, ubtAddress, point, pointType) {
+async function increaseUBT (requestParam)
+{
   var domain = CONFIG.ubtDomain
   return new Promise((resolve, reject) => {
-    wx.request({
-      url: 'http://' + domain + '/ubt/point/increase', //增加积分；
-      data: {
-        "point": point,
-        "seq": Math.round(Math.random() * 1000000),
-        "name": ubtAddress,
-        "type": pointType,
-        "uid": uid
-      },
-      method: "POST",
-      header: {
-        "Content-Type": "application/json"
-      },
-      complete: function (res) {
-        if (res == null || res.data == null) {
+  wx.request({
+    url: 'http://' + domain +'/ubt/point/increase', //增加积分；
+    data: requestParam,
+    method: "POST",
+    header: {
+        "Content-Type": "application/json" 
+    },
+    complete: function( res ) {
+        if( res == null || res.data == null) {
+          console.error('网络请求失败')
           reject(new Error('网络请求失败'))
         }
 
-      },
-      success: function (res) {
+    },
+    success: function(res) {
 
-        if (res.data.status == 0) {
-          resolve(res.data)
-        }
-        else {
-          reject(new Error(res));
-        }
-      }
-    })
+      if(res.data.status == 0){
+        console. info('Successfully increase Point ' + point + 'for user ' + uid + ' pointType ' + pointType);
+        resolve(res.data)
+    }
+    else
+    {
+      reject(new Error(res));
+    }
+    }
   })
+})
 }
 
-async function createAccount(uid, point, pointType) {
+async function createAccount (uid,point,pointType)
+{
   var domain = CONFIG.ubtDomain
   return new Promise((resolve, reject) => {
-    wx.request({
-      url: 'http://' + domain + '/ubt/point/create', //创建帐号；
-      data: {
-        "point": point,
-        "seq": Math.round(Math.random() * 1000000),
-        "type": pointType,
-        "uid": uid
-      },
-      method: "POST",
-      header: {
-        "Content-Type": "application/json"
-      },
-      complete: function (res) {
-        if (res == null || res.data == null) {
+  wx.request({
+    url: 'http://' + domain +'/ubt/point/create', //创建帐号；
+    data: {
+      "point": point,
+      "seq": Math.round(Math.random() * 1000000),
+      "type": pointType,
+      "uid": uid
+    },
+    method: "POST",
+    header: {
+        "Content-Type": "application/json" 
+    },
+    complete: function( res ) {
+        if( res == null || res.data == null) {
+          console.error('网络请求失败')
           reject(new Error('网络请求失败'))
         }
 
-      },
-      success: function (res) {
+    },
+    success: function(res) {
 
-        if (res.data.status == 0) {
-          resolve(res.data)
-        }
-        else {
-          reject(new Error(res));
-        }
-      }
-    })
+      if(res.data.status == 0){
+        console. info('Successfully Create Account for user: ' + uid + ', pointType: ' + pointType);
+        resolve(res.data)
+    }
+    else
+    {
+      reject(new Error(res));
+    }
+    }
   })
+})
 }
 
 
 module.exports = {
-  retrieveUBT: retrieveUBT,
+  retrieveUBT:retrieveUBT,
   checkAndCreateUser: checkAndCreateUser,
   increaseUBT: increaseUBT,
   decreaseUBT: decreaseUBT,
-  exchangeScoreToUBT: exchangeScoreToUBT
+  exchangeScoreToUBT: exchangeScoreToUBT,
+  createScoreParam: createScoreParam,
+  createUBTParam: createUBTParam
 }
